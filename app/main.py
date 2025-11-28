40aac7696dddae24ced1266e30823da5b77c1eb5
Tamer Zraiq <118359972+TamerZraiq@users.noreply.github.com>
2025-11-28 12:19:51 +0000

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
+from fastapi import FastAPI
+app = FastAPI(title="Service A - Greeting API")
 
-from app.database import engine, SessionLocal
-from app.models import Base, UserDB
-from app.schemas import UserCreate, UserRead
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
-app = FastAPI()
-Base.metadata.create_all(bind=engine)
-
+@app.get("/api/greet/{name}")
+def greet(name: str):
+    return {"message": f"Hello, {name} from Service A!"}
